0a76ac4782967aa5104603b95c3669ce65503e7d
zhyhe <zhyhe@outlook.com>
2020-12-15 10:36:07 +0800

    20201215

--- Fortran/h/h.F90
+++ Fortran/h/h.F90
@@ -1,6 +1,22 @@
 !这是一个无限死循环程序
-        integer::i=0
-        do
-        i=i+1
-        enddo
-        end
+        implicit none
+        integer,parameter :: n=50
+        integer :: i, a(3*n,n)
+
+        a=0
+        a(35,35)=1
+        a(36,36)=1
+        a(35,36)=1
+        a(36,35)=1
+
+        print'(150I1)',a
+        do j=2,n-1
+        do i=2,3*n-1
+                do l=-1,1
+                do k=-1,1
+                s=s+a(i+k,j+l)
+                enddo
+                enddo
+
+
+end
